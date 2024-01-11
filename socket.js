8f30099385441a6114613bce858084a93bd98a35
artt05 <artthaqi05@gmail.com>
2024-01-11 16:53:17 +0100

    sockets not finished

--- socket.js
+++ socket.js
@@ -1,24 +1,52 @@
 function listen(io) {
   const chatNamespace = io.of("/messages");
+  const connectedUsers = {};
 
   chatNamespace.on("connection", (socket) => {
-    console.log("User connected");
+    console.log("User connected", socket.id);
 
-    socket.on("join", (userId) => {
-      // Join a room specific to the userId
-      socket.join(userId);
-      console.log(`User with ID ${userId} joined the chat`);
-    });
+    connectedUsers[socket.id] = { connected: true, typing: false };
 
-    socket.on("message", ({ userId, message }) => {
-      // Broadcast the message to the specific user
+    // Emit 'join' event to notify user presence
+    socket.emit("join", { userId: socket.id, connectedUsers });
+    socket.on("message", ({ userId, senderId, message }) => {
+      console.log(message);
+      console.log("userId", userId);
+      console.log("senderId", senderId);
+      console.log("socket.id", socket.id);
+
+      // Create a room name based on the userId and senderId
+      const roomName = `${userId}-${senderId}`;
+
+      // Join the room for both sender and receiver
+      socket.join(roomName);
+
+      // Emit the message to the room
       chatNamespace
-        .to(userId)
-        .emit("message", { senderId: socket.id, message });
+        .to(roomName)
+        .emit("message", { senderId: senderId, message });
+      console.log("message sent");
+    });
+    socket.on("typing", ({ userId, isTyping }) => {
+      connectedUsers[socket.id].typing = isTyping;
+
+      // Create a room name based on the userId and senderId
+      const roomName = `${userId}-${socket.id}`;
+
+      // Join the room
+      socket.join(roomName);
+
+      // Emit the typing event to the room
+      chatNamespace
+        .to(roomName)
+        .emit("typing", { senderId: socket.id, isTyping });
+      console.log("typing sent");
     });
 
     socket.on("disconnect", () => {
       console.log("User disconnected");
+      delete connectedUsers[socket.id];
+      socket.broadcast.emit("join", { connectedUsers });
     });
   });
 }
