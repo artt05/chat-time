20112d59f07349f8fecc9cd1f6e5ac05720df5ee
artt05 <artthaqi05@gmail.com>
2024-01-12 16:20:16 +0100

    live form fixed

--- socket.js
+++ socket.js
@@ -1,52 +1,55 @@
+// Import the Socket.io library
+
 function listen(io) {
   const chatNamespace = io.of("/messages");
   const connectedUsers = {};
 
   chatNamespace.on("connection", (socket) => {
     console.log("User connected", socket.id);
-
+    let roomName = "";
     connectedUsers[socket.id] = { connected: true, typing: false };
 
     // Emit 'join' event to notify user presence
-    socket.emit("join", { userId: socket.id, connectedUsers });
-    socket.on("message", ({ userId, senderId, message }) => {
-      console.log(message);
-      console.log("userId", userId);
-      console.log("senderId", senderId);
-      console.log("socket.id", socket.id);
-
-      // Create a room name based on the userId and senderId
-      const roomName = `${userId}-${senderId}`;
+    socket.broadcast.emit("join", { userId: socket.id, connectedUsers });
+    socket.on("ready", ({ userId, senderId }) => {
+      roomName = `ski aty `;
 
       // Join the room for both sender and receiver
       socket.join(roomName);
+      console.log("Joined room:", roomName);
+    });
+    socket.on("message", ({ userId, senderId, message }) => {
+      console.log("Received message:", message);
+
+      // Create a room name based on the userId and senderId
 
       // Emit the message to the room
       chatNamespace
         .to(roomName)
         .emit("message", { senderId: senderId, message });
+
       console.log("message sent");
     });
-    socket.on("typing", ({ userId, isTyping }) => {
-      connectedUsers[socket.id].typing = isTyping;
 
+    socket.on("typing", ({ senderId, isTyping }) => {
+      connectedUsers[socket.id].typing = isTyping;
+      console.log("Received typing:", isTyping);
       // Create a room name based on the userId and senderId
-      const roomName = `${userId}-${socket.id}`;
 
       // Join the room
-      socket.join(roomName);
 
       // Emit the typing event to the room
       chatNamespace
         .to(roomName)
-        .emit("typing", { senderId: socket.id, isTyping });
+        .emit("typing", { senderId: senderId, isTyping });
+
       console.log("typing sent");
     });
 
     socket.on("disconnect", () => {
       console.log("User disconnected");
       delete connectedUsers[socket.id];
-      socket.broadcast.emit("join", { connectedUsers });
+      chatNamespace.emit("join", { connectedUsers });
     });
   });
 }
