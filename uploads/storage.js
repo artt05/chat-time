2674a42148ccc12b13f2f8e478d092580df08595
artt05 <artthaqi05@gmail.com>
2024-01-16 11:05:59 +0100

    j

--- uploads/storage.js
+++ uploads/storage.js
@@ -3,7 +3,7 @@ const path = require("path");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../chat-time/uploads/uploads/"); // Specify the directory where you want to store uploaded files
+    cb(null, "./uploads/uploads/"); // Specify the directory where you want to store uploaded files
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // Rename the file with a unique name
